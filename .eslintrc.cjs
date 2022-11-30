17f42b8ba1ac5800b43010966327ada2cdad4d47
Hallecsy <alexismuguetpro@gmail.com>
2022-11-30 09:44:15 +0100

    ➕ Ajout d'Eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": [
+        "airbnb-base",
+        "airbnb-typescript/base"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json"
+    },
+}
